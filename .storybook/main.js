919ac781eee454b2ebfef9a720041ffc9416837d
coolwind0202 <tororo_shidoro@yahoo.co.jp>
2022-04-02 23:28:45 +0900

    [fix] storybookが動作するよう修正 ・Themeが反映されるようpreview.jsを修正 ・Noto Sansが読み込まれるよう修正

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
   typescript: { reactDocgen: false },
-  stories: '../src/**/*.stories.@(tsx|mdx)',
+  stories: ['../src/**/*.stories.@(tsx|mdx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
@@ -9,5 +9,13 @@ module.exports = {
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-webpack5',
+  },
+  webpackFinal: async (config) => {
+    // MUI v5 に伴うエラーの回避
+    // https://github.com/mui/material-ui/issues/24282#issuecomment-951015101
+    delete config.resolve.alias['emotion-theming'];
+    delete config.resolve.alias['@emotion/styled'];
+    delete config.resolve.alias['@emotion/core'];
+    return config;
   },
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,20 @@
+import theme from '../src/mui_config/theme';
+
+import { ThemeProvider } from '@mui/material/styles';
+import '../src/styles/globals.css';
+import CssBaseline from '@mui/material/CssBaseline';
+
+export const decorators = [
+  (Story) => {
+    return (
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Story />
+      </ThemeProvider>
+    );
+  },
+];
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
