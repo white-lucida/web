8ad32ee8ec185566e5edc3c87e040fc41930338b
coolwind0202 <tororo_shidoro@yahoo.co.jp>
2022-04-03 09:20:25 +0900

    [style] format

--- src/mui_config/theme.ts
+++ src/mui_config/theme.ts
@@ -1,24 +1,28 @@
-import { createTheme } from "@mui/material/styles";
+import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#86c3ff",
-      contrastText: "white",
+      main: '#86c3ff',
+      contrastText: 'white',
     },
     warning: {
-      main: "#ff8686",
-      contrastText: "white",
+      main: '#ff8686',
+      contrastText: 'white',
+    },
+    info: {
+      main: '#707070',
+      contrastText: 'white',
     },
     text: {
-      primary: "#707070",
+      primary: '#707070',
     },
   },
   typography: {
     h1: {
       fontFamily: '"Noto Sans JP", sans-serif',
       fontWeight: 900,
-      fontSize: "1.2rem",
+      fontSize: '1.2rem',
     },
     button: {
       fontFamily: '"Noto Sans JP", sans-serif',
